efaac0e1c3dbee524c54ab0e80c643b35c3e83e7
Elizeu <loopsdesenvolvedor@gmail.com>
2025-04-02 11:06:01 -0300

    feat: add function to format currencies to real

---
+++ src/utils/FormatValue.ts
@@ -0,0 +1,8 @@
+// função para formatar moeda.
+
+export const formatValue = (value: number) =>
+  new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+    minimumFractionDigits: 2,
+  }).format(value);
